ba33c0692b74fa2740433f6986a60a89d87afca8
Matt Torrence <matt@torrencefamily.net>
2017-09-07 13:09:26 -0400

    Update GameCore.py

--- GameCore.py
+++ GameCore.py
@@ -248,7 +248,6 @@ def cGame(stdscr = None, p1ai = None, p2ai = None):
 
 if __name__ == '__main__':
     from sys import argv
-    from AI import doubleProbDropAI, hyperScoreAI
     try:
         argv[1]
     except:
@@ -258,8 +257,10 @@ if __name__ == '__main__':
     if t == 'local':
         cGame()
     elif t == 'hscore':
+        from AI import hyperScoreAI
         ai = hyperScoreAI(2)
         cGame(p2ai=ai)
     elif t == 'dprop':
+        from AI import doublePropDropAI
         ai = doubleProbDropAI(2)
         cGame(p2ai=ai)
